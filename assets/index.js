4849123e2a7d8caa70c6054874aa3ff308e33418
Caio <caiofrancoramineli3@gmail.com>
2024-02-05 23:48:00 -0300

    inicio pronto

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const palavra = '- Full Stack.';
+    const elementoTexto = document.getElementById('fullstack');
+
+    function escreverLetraPorLetra() {
+      let index = 0;
+  
+      const intervalo = setInterval(function() {
+        elementoTexto.textContent += palavra[index];
+        index++;
+        if (index === palavra.length) {
+          clearInterval(intervalo);
+          setTimeout(function() {
+            elementoTexto.textContent = '';
+            escreverLetraPorLetra();
+          }, 1500);
+        }
+      }, 200);
+    }
+    escreverLetraPorLetra();
+});
