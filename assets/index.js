9fb3bdb58715fd65e8dedc7c9e59f2e15a61433a
Caio <caiofrancoramineli3@gmail.com>
2024-02-14 23:53:30 -0300

    sessao de tecnologias pronta

--- assets/index.js
+++ assets/index.js
@@ -11,13 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
         if (index === palavra.length) {
           clearInterval(intervalo);
           setTimeout(function() {
-            elementoTexto.textContent = '';
+            apagarLetraPorLetra();
+          }, 1500);
+        }
+      }, 150);
+    }
+
+    function apagarLetraPorLetra() {
+      let index = palavra.length - 1;
+  
+      const intervalo = setInterval(function() {
+        elementoTexto.textContent = elementoTexto.textContent.slice(0, -1);
+        index--;
+        if (index < 0) {
+          clearInterval(intervalo);
+          setTimeout(function() {
             escreverLetraPorLetra();
           }, 1500);
         }
-      }, 200);
+      }, 100);
     }
     escreverLetraPorLetra();
 });
-
-
