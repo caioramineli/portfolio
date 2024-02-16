c8a7b36a5c8655036c7d6899f86a81c9e41d7038
Caio <caiofrancoramineli3@gmail.com>
2024-02-15 23:55:58 -0300

    adicionar modal e melhorias

--- assets/index.js
+++ assets/index.js
@@ -3,23 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const elementoTexto = document.getElementById('fullstack');
 
     function escreverLetraPorLetra() {
-      let index = 0;
-  
-      const intervalo = setInterval(function() {
-        elementoTexto.textContent += palavra[index];
-        index++;
-        if (index === palavra.length) {
-          clearInterval(intervalo);
-          setTimeout(function() {
-            apagarLetraPorLetra();
-          }, 1500);
-        }
-      }, 150);
-    }
+        let index = 0;
+
+        const intervalo = setInterval(function() {
+          elementoTexto.textContent += palavra[index];
+          index++;
+          if (index === palavra.length) {
+            clearInterval(intervalo);
+            setTimeout(function() {
+              apagarLetraPorLetra();
+            }, 1500);
+          }
+        }, 150);
+      }
 
     function apagarLetraPorLetra() {
       let index = palavra.length - 1;
-  
+
       const intervalo = setInterval(function() {
         elementoTexto.textContent = elementoTexto.textContent.slice(0, -1);
         index--;
@@ -33,3 +33,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     escreverLetraPorLetra();
 });
+
+function abrirModal() {
+    document.getElementById('modal').style.display = 'inline';
+}
+
+function fecharModal() {
+  document.getElementById('modal').style.display = 'none';
+}
