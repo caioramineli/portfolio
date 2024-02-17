057b3909dda0567a4d86c437d393e79597c79423
Caio <caiofrancoramineli3@gmail.com>
2024-02-17 16:11:24 -0300

    modal de contato finalizado

--- assets/index.js
+++ assets/index.js
@@ -34,10 +34,48 @@ document.addEventListener('DOMContentLoaded', function() {
     escreverLetraPorLetra();
 });
 
+var input = document.getElementsByClassName('input')
+
 function abrirModal() {
-    document.getElementById('modal').style.display = 'inline';
+    document.getElementById('modal').style.display = 'flex';
+    input[0].focus();
 }
 
 function fecharModal() {
-  document.getElementById('modal').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('span-form').style.display = 'none';
 }
+
+function tirarSpanForm() {
+    document.getElementById('span-form').style.display = 'none';
+}
+
+var form = document.getElementById("form");
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("span-form");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.style.display = "inline";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Houve um problema ao enviar o email. Porfavor tente novamente!"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Houve um problema ao enviar o email. Porfavor tente novamente!"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
